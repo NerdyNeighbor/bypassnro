1aab954ca45e1af8a40632207de55de6402ce4aa
NerdyNeighbor <128112486+NerdyNeighbor@users.noreply.github.com>
2026-02-16 17:02:34 -0700

    Update bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,2 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/NerdyNeighbor/bypassnro/refs/heads/main/unattend.xml
+curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/NerdyNeighbor/bypassnro/refs/heads/main/unattend-new.xml
 %WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
